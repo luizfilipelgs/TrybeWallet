80291c1ae9384cba37a7a97e4e6d487f0f612fe9
Luiz Filipe <luizfilipelgs@gmail.com>
2022-08-29 22:46:20 -0300

    inicio do projeto, confg inicial e inicio req 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,59 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    senha: '',
+    btnDisabled: true,
+  };
+
+  validateLogin = () => {
+    const { email, senha } = this.state;
+    const minLengthSenha = 5;
+    const regexEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi.test(email);
+    const stateBolean = regexEmail && senha.length >= minLengthSenha;
+    console.log(!stateBolean);
+    return !stateBolean; // tem que retornar false
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value, btnDisabled: this.validateLogin });
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, senha, btnDisabled } = this.state;
+    return (
+      <main>
+        <h1>Acesso</h1>
+        <form>
+          <label htmlFor="email-input">
+            Login:
+            <input
+              type="email"
+              name="email"
+              data-testid="email-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="email-input">
+            Senha:
+            <input
+              type="text"
+              name="senha"
+              data-testid="password-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="submit"
+            onClick={ this.handleChange }
+            disabled={ btnDisabled }
+          >
+            Entrar
+          </button>
+        </form>
+      </main>
+    );
   }
 }
 
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '', // string que armazena o email da pessoa usuária
+  },
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return {
+      ...state,
+      ...action.payload,
+    };
+  }
+};
+
+export default user;
