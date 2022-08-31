0c651ebd84c0ec18f81447f644b42cd2fb5698ef
Luiz Filipe <luizfilipelgs@gmail.com>
2022-08-30 22:07:16 -0300

    req 2

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <p data-testid="email-field">{`Email: ${email}`}</p>
+        <p data-testid="total-field">
+          {`Despesa Total: ${0} `}
+          <span data-testid="header-currency-field">BRL</span>
+        </p>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
---
+++ src/pages/Wallet.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+
+class Wallet extends React.Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <main>oi</main>
+      </>
+    );
+  }
+}
+
+export default Wallet;
